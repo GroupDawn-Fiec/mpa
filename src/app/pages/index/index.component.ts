b54a6a44ad27d31373e5ab869d604e3d7d59af03
Angel T <aatomala@espol.edu.ec>
2023-12-16 17:40:42 -0500

    add something

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterLinkActive, RouterLink } from '@angular/router';
+import { DataProviderService } from '../../providers/data-provider.service';
+import { HttpClientModule } from  '@angular/common/http';
+import { Game } from '../../interfaces/game';
 
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [ RouterLinkActive, RouterLink],
+  imports: [ RouterLinkActive, RouterLink, HttpClientModule],
+  providers: [DataProviderService],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
 export class IndexComponent {
-
+  public data : Game[] = [];
+  constructor(private dataProvider: DataProviderService) { }
+  ngOnInit() {
+    this.dataProvider.getResponse().subscribe((response) => { 
+      let dataArray = (response as Game[]); 
+      this.data = dataArray.slice(0,20);
+    },(error) => {
+      console.error('Error en la petición:', error);
+    })
+  }
 }
