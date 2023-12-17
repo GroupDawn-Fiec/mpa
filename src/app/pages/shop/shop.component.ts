82c7417cf737b85c4560d5a3a0adeeef2c15c1bc
Angel T <aatomala@espol.edu.ec>
2023-12-17 12:25:06 -0500

    modify home and contact page, data added to DataSet

--- src/app/pages/shop/shop.component.ts
+++ src/app/pages/shop/shop.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterLinkActive, RouterLink } from '@angular/router';
+import { HttpClientModule } from  '@angular/common/http';
+import { Game } from '../../interfaces/game';
+import { DataProviderService } from '../../providers/data-provider.service';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [ RouterLinkActive, RouterLink],
+  imports: [ RouterLinkActive, RouterLink, HttpClientModule],
+  providers: [ DataProviderService ],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.css'
 })
 export class ShopComponent {
+
+  public data : Game[] = [];
+  constructor(private dataProvider: DataProviderService) { }
+  ngOnInit() {
+    this.dataProvider.getResponse().subscribe((response) => { 
+      let dataArray = (response as Game[]); 
+      this.data = dataArray.slice(0,20);
+    },(error) => {
+      console.error('Error en la petición:', error);
+    })
+  }
 
 }
