46d462bf30cb1edca3de2fd793cd97e7d08fd07f
K3vR0ld4n <kroldan@espol.edu.ec>
2023-12-18 20:03:22 -0500

    pages in table added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,13 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HttpClientModule } from  '@angular/common/http';
 
+import { NgxPaginationModule } from 'ngx-pagination';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent, HttpClientModule],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent, HttpClientModule,NgxPaginationModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
