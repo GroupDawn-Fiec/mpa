d5be8c752d519fc8485cb1b8d7fb45cb868ffe63
JorgeMawyin <jmawyin@espol.edu.ec>
2023-12-23 18:41:18 -0500

    add: library for icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,19 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { HttpClientModule } from  '@angular/common/http';
-
+import { HttpClientModule } from '@angular/common/http';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent, HttpClientModule,NgxPaginationModule],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent, HttpClientModule, NgxPaginationModule, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'mpa3';
-  
+
 }
